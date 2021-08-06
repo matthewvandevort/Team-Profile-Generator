9569ad154d4b66dfaeb7c8ced35965b374b3e91c
Matthew Vandevort <vandevortmatthew@gmail.com>
2021-08-06 14:11:40 -0400

    got Intern test to pass

---
+++ _tests_/Intern.test.js
@@ -0,0 +1,26 @@
+const Intern = require('../lib/Intern');
+
+const internTest = new Intern ('Peter Parker', 'spidey@gmail.com', 4, 'Intern', 'Empire State U');
+
+describe('Intern', () => {
+    it('has a name', () => {
+        expect(internTest.name).toEqual(expect.any(String));
+        expect(internTest.name.length).toBeGreaterThan(2);
+    })
+
+    it('has an email address', () => {
+        expect(internTest.email).toEqual(expect.stringContaining('@'));
+    });
+
+    it('has an id that is a number', () => {
+        expect(internTest.id).toEqual(expect.any(Number));
+    });
+
+    it('has a role of Intern', () => {
+        expect(internTest.role).toBe('Intern');
+    });
+
+    it('has a school name', () => {
+        expect(internTest.schoolName).toEqual(expect.any(String));
+    });
+});

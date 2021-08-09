f91017656b55611845a99b363c3f117a46967b98
Matthew Vandevort <vandevortmatthew@gmail.com>
2021-08-09 12:08:07 -0400

    added questions and finalized functions

--- index.js
+++ index.js
@@ -42,5 +42,127 @@ const employeeQuest = [
             'Engineer', 'Intern', 'I am done adding team members.',
         ],
     },
-  
+    
 ],
+
+const engineeerQuest = [
+    {
+        type: 'input',
+        message: 'Please enter the engineers name.',
+        name: 'engineerName',
+    },
+    {
+        type: 'input',
+        message: 'Please enter the engineers id number.',
+        name: 'engineerId',
+    },
+    {
+        type: 'input',
+        message: 'Please enter the engineers e-mail address.',
+        name: 'engineerEmail',
+    },
+    {
+        type: 'input',
+        message: 'Please enter the engineers GitHub username.',
+        name: 'engineerGithub',
+    },
+    {
+        type: 'list',
+        message: 'What type of team member would you like to add next?',
+        name: 'teamMember',
+        choices: [
+            'Engineer', 'Intern', 'I am done adding team members.',
+        ],
+    },
+    
+]
+
+const internQuest = [
+    {
+        type: 'input',
+        message: "Please enter the intern's name.",
+        name: 'internName',
+    },
+    {
+        type: 'input',
+        message: "Please enter the intern's id number.",
+        name: 'internId',
+    },
+    {
+        type: 'input',
+        message: "Please enter the intern's e-mail address.",
+        name: 'internEmail',
+    },
+    {
+        type: 'input',
+        message: "Please enter the intern's school.",
+        name: 'internSchool',
+    },
+    {
+        type: 'list',
+        message: 'What type of team member would you like to add next?',
+        name: 'teamMember',
+        choices: [
+            'Engineer', 'Intern', 'I am done adding team members.',
+        ],
+    },
+    
+]
+
+function engineerQuestSort() {
+    inquirer
+        .prompt(engineeerQuest)
+        .then(answers => {
+            let engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub,);
+            teamMembers.push(engineer);
+            if (answers.teamMember === 'Engineer') {
+                engineerQuestSort();                
+            } else if (answers.teamMember === 'Intern') {
+                internQuestSort();
+            } else {
+                makeHtml();
+            }
+        })
+}
+
+function internQuestSort() {
+    inquirer
+        .prompt(internQuest)
+        .then(answers => {
+            let intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool,);
+            teamMembers.push(intern);
+            if (answers.teamMember === 'Engineer') {
+                engineerQuestSort();                
+            } else if (answers.teamMember === 'Intern') {
+                internQuestSort();
+            } else {
+                makeHtml();
+            }
+        })
+}
+
+function init(){
+    inquirer
+        prompt(employeeQuest)
+        .then(answers => {
+            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNum);
+            teamMembers.push(manager);
+            if (answers.teamMember === 'Engineer') {
+                console.log('It is an engineer');
+                engineerQuestSort();
+            } else if (answers.teamMember === 'Intern') {
+                console.log('It is an intern.');
+                internQuestSort();
+            } else {
+                makeHtml();
+            }
+        })
+}
+
+function makeHtml() {
+    const makeHtml = fs.watchFile(outputLocation, render(teamMembers), (err) => {
+        err ? console.log(err) : console.log('Succesfully generated the HTML!');
+    })
+}
+
+init();

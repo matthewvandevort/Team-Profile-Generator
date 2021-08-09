d1ebd605714a35a59954735879de672eb744f0c6
Matthew Vandevort <vandevortmatthew@gmail.com>
2021-08-09 11:33:45 -0400

    started the index.js file

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
 const fs = require('fs');
+const path = require('path');
 const inquirer = require('inquirer');
 const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
 const Engineer = require('./lib/Engineer');
 
+const OUTPUT_DIR = path.resolve(__dirname, 'output');
+const outputLocation = path.join(OUTPUT_DIR, 'team.html');
 
-function getUserInput() {
-    inquirer.prompt
-}
+const render = require('./lib/renderHtml');
+const teamMembers = [];
+
+
+const employeeQuest = [
+    {
+        type: 'input',
+        message: 'Please enter the team managers name.',
+        name: 'managerName',
+    },
+    {
+        type: 'input',
+        message: 'Please enter the managers id number.',
+        name: 'managerId',
+    },
+    {
+        type: 'input',
+        message: 'Please enter the managers e-mail address.',
+        name: 'managerEmail',
+    },
+    {
+        type: 'input',
+        message: 'Please enter the managers office number.',
+        name: 'managerOfficeNum',
+    },
+    {
+        type: 'list',
+        message: 'What type of team member would you like to add next?',
+        name: 'teamMember',
+        choices: [
+            'Engineer', 'Intern', 'I am done adding team members.',
+        ],
+    },
+  
+],

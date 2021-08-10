d5902047a09a3b62a51fd7cce5c3493d560624a2
Matthew Vandevort <vandevortmatthew@gmail.com>
2021-08-10 16:20:24 -0400

    general fixes

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 const inquirer = require('inquirer');
-const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
 const Engineer = require('./lib/Engineer');
